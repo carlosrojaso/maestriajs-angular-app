81553a700a50c86e6732819c6bd3eda51d1847ec
Carlos Rojas <carlos.rojas@localhost>
2019-12-19 06:52:52 -0500

    Angular 9.rc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MaestriajsAngularApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.0-rc.6.
 
 ## Development server
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to maestriajs-angular-app!');
+    expect(page.getTitleText()).toEqual('maestriajs-angular-app app is running!');
   });
 
   afterEach(async () => {
